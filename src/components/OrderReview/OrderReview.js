cd843ba5cc05c66e8d51622b3ab9ed21d3cacb3e
tithi23 <tithiratry@gmail.com>
2021-10-03 22:55:23 -0700

    components added and ema jhon order review problem

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
+import useCart from '../hooks/useCart';
+import useProducts from '../hooks/useProducts';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
+import { useHistory } from 'react-router';
 
 const OrderReview = () => {
+    const [products] = useProducts();
+    const [cart, setCart] = useCart(products);
+    const handleRemove = key => {
+        const newCart = cart.filter(product => product.key !== key)
+        setCart(newCart);
+        removeFromDb(key);
+    }
+    const history = useHistory()
+    const handlePlaceOrder = () => {
+        history.push('/placeorder');
+        clearTheCart();
+    }
     return (
-        <div>
-            <h2>This is Order Review</h2>
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.key}
+                        product={product}
+                        handleRemove={handleRemove}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <button onClick={handlePlaceOrder} className="btn-regular">Place order</button>
+                </Cart>
+            </div>
         </div>
     );
 };
